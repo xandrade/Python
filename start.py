f5f2d3ce0d8ec93a9c3eaec6df3ea718c58892f1
xandrade <andrade.antonio@gmail.com>
2019-05-10 21:39:40 +0400

    First version

--- start.py
+++ start.py
@@ -1,17 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+start.py - v0.0.1 2019-05-10 Antonio Andrade
+This module provides simple classes and interfaces
+Author's website: http://
+"""
+__version_info__ = (0, 0, 1)
+__version__ = '.'.join(map(str, __version_info__))
+__author__ = "Antonio Andrade"
+__copyright__ = "Copyright 2019, Antonio Andrade"
+__credits__ = ["Antonio Andrade", ]
+__license__ = "Apache 2.0"
+__maintainer__ = "Antonio Andrade"
+__email__ = "andrade.antonio@gmai.com"
+__status__ = "Development"
+
+### CHANGES ###
+# 2019-05-10: v0.0.1 AA:
+#   - First version
+
+### TODO ###
+#
+
+### REFERENCES ###
+#
+
+
 def testing_var_1():
+
     return 1
+
 
 def testing_var_2():
     return 2
 
+
 def testing_failed():
     return None
 
-def testing(var):
-    if var == 1: return testing_var_1()
-    if var == 2: return testing_var_2()
-    else:  return testing_failed()
 
-print(testing(1))
-print(testing(3))
-print(testing(22))
+def testing(var):
+    if var == 1:
+        return testing_var_1()
+    if var == 2:
+        return testing_var_2()
+    else:
+        return testing_failed()
+
+
+if __name__ == "__main__":
+
+    print(testing(1))
+    print(testing(3))
+    print(testing(22))
